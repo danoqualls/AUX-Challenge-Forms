0897f109107d5714e86193139620a5d24a4c8aff
danoqualls <mynameisdano@gmail.com>
2015-03-18 14:40:26 -0400

    working credit card validation in js

--- build/assets/js/script.js
+++ build/assets/js/script.js
@@ -1,1 +1,26 @@
-/* Your JavaScript goes here */
+var $amex = $("#American-Express"),
+$visa = $("#Visa"),
+$discover = $("#Discover"),
+$mastercard = $("#MasterCard");
+
+$("#card-number").validateCreditCard(function(result) {
+	console.log(result.card_type.name);
+
+	if ( result.card_type.name === "amex" ) {
+
+		$amex.attr("checked",true);
+
+	} else if ( result.card_type.name === "visa" ) {
+
+		$visa.attr("checked",true);
+
+	} else if ( result.card_type.name === "discover" ) {
+
+		$discover.attr("checked",true);
+
+	} else if ( result.card_type.name === "mastercard" ) {
+
+		$mastercard.attr("checked",true);
+	};
+});
+
